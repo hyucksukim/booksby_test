ba46a336e5a4718a7b0c1a3d497a475412c66437
hyucksukim <86274940+hyucksukim@users.noreply.github.com>
2022-06-09 03:58:46 +0900

    Update app.py

--- Dockerfiles/Backend/app/app.py
+++ Dockerfiles/Backend/app/app.py
@@ -159,7 +159,7 @@ def get_item(asin):
 @app.get("/title/{asin}")
 def get_title(asin):
     items = get_item(asin)
-    return items[0]['title55555']
+    return items[0]['title666666']
     
     
 if __name__ == "__main__":
